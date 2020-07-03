c5c93a0dda9d30aeb4c43d7e21633f3522e514d2
magnuslopes23 <lopesmagnus2324@gmail.com>
2020-07-03 19:34:31 +0530

    data binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+import { from } from "rxjs";
 export class Dish{
     id: string;
     name: string;
@@ -7,4 +9,5 @@ export class Dish{
     label: string;
     price: string;
     description: string;
+    comments:Comment[];
 }
