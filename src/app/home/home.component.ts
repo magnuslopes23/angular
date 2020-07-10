c0551448ac4a14ff1fae028a7d3933d7816b9449
magnuslopes23 <lopesmagnus2324@gmail.com>
2020-07-10 12:12:13 +0530

    single page application part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
 
+import { Promotion } from '../shared/promotion';
+
+import { DishService } from '../services/dish.service';
+import { PromotionService } from '../services/promotion.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  dish:Dish;
+  promotion:Promotion;
 
-  constructor() { }
+  constructor(private dishservice:DishService, private promotionservive:PromotionService) { }
 
   ngOnInit() {
+    this.dish=this.dishservice.getFeaturedDish();
+    this.promotion=this.promotionservive.getFeaturedPromotion();
   }
 
 }
